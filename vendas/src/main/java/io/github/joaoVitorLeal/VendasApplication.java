ed240986c48927f76496e6f4e7b1e6ef59acb1b0
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-14 21:55:35 -0300

    feat: Iniciando preparação do ambiente para o desenvolvimento dos controllers REST, removido código de teste do Application

--- vendas/src/main/java/io/github/joaoVitorLeal/VendasApplication.java
+++ vendas/src/main/java/io/github/joaoVitorLeal/VendasApplication.java
@@ -1,33 +1,10 @@
 package io.github.joaoVitorLeal;
 
-import io.github.joaoVitorLeal.domain.entity.Cliente;
-import io.github.joaoVitorLeal.domain.repository.ClienteRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner init(@Autowired ClienteRepository clienteRepository) {
-        return args -> {
-            System.out.println("Salvando clientes.");
-            clienteRepository.save(new Cliente("João"));
-            clienteRepository.save(new Cliente("Manuela"));
-
-
-            String nome = "João";
-            List<Cliente> clientes = clienteRepository.SQLEncontrarPorNome(nome);
-            clientes.forEach(System.out::println);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
