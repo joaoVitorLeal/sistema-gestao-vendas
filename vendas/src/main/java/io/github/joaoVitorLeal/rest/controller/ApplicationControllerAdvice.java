12fdf88b873b7e8f48063fb5bb50e280726c9ecd
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-16 13:41:48 -0300

    feat(api): adicionar tratamento global de exceções e estrutura de resposta a erros

---
+++ vendas/src/main/java/io/github/joaoVitorLeal/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package io.github.joaoVitorLeal.rest.controller;
+
+import io.github.joaoVitorLeal.exception.RegraNegocioException;
+import io.github.joaoVitorLeal.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Tratamento de erros utilizando Exceptions Handles
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class) // marca o méto-do como tratador de erros
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException (RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
