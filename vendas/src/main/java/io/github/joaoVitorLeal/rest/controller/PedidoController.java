fb696e362a70e4f8559ed9fb30e36f07971f7c46
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-16 10:46:40 -0300

    feat (controller-pedido): Implementado o controller para gestão de Pedido.

---
+++ vendas/src/main/java/io/github/joaoVitorLeal/rest/controller/PedidoController.java
@@ -0,0 +1,16 @@
+package io.github.joaoVitorLeal.rest.controller;
+
+import io.github.joaoVitorLeal.service.PedidoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidoController {
+
+    private final PedidoService service;
+
+    public PedidoController(PedidoService service) {
+        this.service = service;
+    }
+}
