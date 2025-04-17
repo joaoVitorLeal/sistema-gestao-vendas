d25f1f8e7a85f12d83f439bfb7d35cc24502ae7e
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-16 21:30:37 -0300

    feat(application-controller-advice): adicionar tratamento de exceção para PedidoNaoEncontradoException

--- vendas/src/main/java/io/github/joaoVitorLeal/rest/controller/ApplicationControllerAdvice.java
+++ vendas/src/main/java/io/github/joaoVitorLeal/rest/controller/ApplicationControllerAdvice.java
@@ -1,5 +1,6 @@
 package io.github.joaoVitorLeal.rest.controller;
 
+import io.github.joaoVitorLeal.exception.PedidoNaoEncontradoException;
 import io.github.joaoVitorLeal.exception.RegraNegocioException;
 import io.github.joaoVitorLeal.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
@@ -13,8 +14,14 @@ public class ApplicationControllerAdvice {
 
     @ExceptionHandler(RegraNegocioException.class) // marca o méto-do como tratador de erros
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiErrors handleRegraNegocioException (RegraNegocioException ex) {
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
         String mensagemErro = ex.getMessage();
         return new ApiErrors(mensagemErro);
+    }
+
+    @ExceptionHandler(PedidoNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrors handlePedidoNaoEncontradoException(PedidoNaoEncontradoException ex) {
+        return new ApiErrors(ex.getMessage());
     }
 }
