79eeb9b1f800549dafe655ba97155fcc534bbc77
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-16 21:06:34 -0300

    feat(pedidos-response-dto): adicionar novos atributos ao DTO.

--- vendas/src/main/java/io/github/joaoVitorLeal/rest/dto/PedidoResponseDTO.java
+++ vendas/src/main/java/io/github/joaoVitorLeal/rest/dto/PedidoResponseDTO.java
@@ -1,10 +1,23 @@
 package io.github.joaoVitorLeal.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder // Gera todas as propriedades como classes Builder, para que possamos construí-las em objetos (evitando primitivos).
 public class PedidoResponseDTO {
-    Integer cliente;
-    BigDecimal total;
-    List<ItemPedidoRequestDTO> itens;
+    private Integer id;
+    private String cpf;
+    private String nomeCliente;
+    private BigDecimal total;
+    private String dataPedido;
+    private String status;
+    private List<ItemPedidoResponseDTO> itens;
 }
