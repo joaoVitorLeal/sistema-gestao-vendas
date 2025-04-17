d2e72c50e22305997db61d24dc5e7694569e478c
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-17 13:08:29 -0300

    feat(api-errors, application-controller-advice): Adicionar nova exceção

--- vendas/src/main/java/io/github/joaoVitorLeal/rest/controller/ApplicationControllerAdvice.java
+++ vendas/src/main/java/io/github/joaoVitorLeal/rest/controller/ApplicationControllerAdvice.java
@@ -4,9 +4,14 @@ import io.github.joaoVitorLeal.exception.PedidoNaoEncontradoException;
 import io.github.joaoVitorLeal.exception.RegraNegocioException;
 import io.github.joaoVitorLeal.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 // Tratamento de erros utilizando Exceptions Handles
 @RestControllerAdvice
@@ -23,5 +28,22 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNaoEncontradoException(PedidoNaoEncontradoException ex) {
         return new ApiErrors(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ApiErrors(errors);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ApiErrors("Requisição malformada: verifique os dados enviados.");
     }
 }
