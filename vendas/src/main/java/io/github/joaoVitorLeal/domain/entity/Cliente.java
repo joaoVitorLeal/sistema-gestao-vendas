b3522537644084fef9b8a436b68e1ccd3bea4fa6
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-22 22:33:34 -0300

    chore(banco-de-dados): adaptando a geração de ID para o SGBD MySQL

--- vendas/src/main/java/io/github/joaoVitorLeal/domain/entity/Cliente.java
+++ vendas/src/main/java/io/github/joaoVitorLeal/domain/entity/Cliente.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true)
     private Integer id;
 
--- vendas/src/main/java/io/github/joaoVitorLeal/domain/entity/Usuario.java
+++ vendas/src/main/java/io/github/joaoVitorLeal/domain/entity/Usuario.java
@@ -16,7 +16,7 @@ import javax.persistence.*;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Integer id;
 
